f399f44384ff94a3c26d2f237f8a6f25b4fdf1cd
Christopher Jones <152762801+christopher-s-jones@users.noreply.github.com>
2024-11-01 16:12:51 -0600

    Include the constants used by the reporter.

---
+++ reporting/constants.go
@@ -0,0 +1,7 @@
+package reporting
+
+// The interval used to poll the Github GraphQL API for contributions
+const PollingIntervalInMinutes = 2
+
+// The default first contribution year
+const DefaultFirstContributionYear = 2000
